634911a0207f83d0170cdda5e373d5e5b9e50846
darya.tashkina@bk.ru <darya.tashkina@bk.ru>
2023-03-19 23:44:58 +0300

    Добавьте файлы проекта.

---
+++ Figurator/Views/Converters/SafeToColor.cs
@@ -0,0 +1,16 @@
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Figurator.Views.Converters {
+    internal class SafeToColor: IValueConverter {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            if (value == null) return null;
+            // if (value is SafePoint @point) return @point.is_valid ? Brushes.Lime : Brushes.Pink;
+            throw new NotSupportedException();
+        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
